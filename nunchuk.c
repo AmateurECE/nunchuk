ff0ada821556e6d1ee4c90da695dcd81e0a50db6
Ethan D. Twardy <ethan.twardy@gmail.com>
2023-04-02 12:20:59 -0500

    Implement Z and C buttons

--- nunchuk.c
+++ nunchuk.c
@@ -6,10 +6,99 @@
 #include <linux/module.h>
 #include <linux/i2c.h>
 #include <linux/input.h>
+#include <linux/delay.h>
 
 struct nunchuk_device {
+	struct i2c_client *client;
 	struct input_dev *input;
 };
+
+struct nunchuk_registers {
+	bool z;
+	bool c;
+};
+
+static int nunchuk_read_registers(struct i2c_client *client,
+				  struct nunchuk_registers *registers)
+{
+	int result = 0;
+	const char read_command[] = { 0x00 };
+	char reg_value[6] = { 0 };
+
+	// For the Nunchuk, each read triggers the next read.
+	// usleep_range(10000, 20000);
+
+	result = i2c_master_send(client, read_command, sizeof(read_command));
+	if (sizeof(read_command) != result) {
+		dev_err(&client->dev,
+			"Failed to send read command to nunchuk!");
+		return result;
+	}
+
+	// usleep_range(9000, 11000);
+	mdelay(2);
+
+	result = i2c_master_recv(client, reg_value, sizeof(reg_value));
+	if (sizeof(reg_value) != result) {
+		dev_err(&client->dev, "Failed to read register from nunchuk!");
+		return result;
+	}
+
+	registers->z = !(reg_value[5] & BIT(0));
+	registers->c = !(reg_value[5] & BIT(1));
+	return 0;
+}
+
+static void nunchuk_poll(struct input_dev *input)
+{
+	int result = 0;
+	struct nunchuk_registers registers = { 0 };
+	struct nunchuk_device *nunchuk = input_get_drvdata(input);
+
+	result = nunchuk_read_registers(nunchuk->client, &registers);
+	if (0 != result) {
+		return;
+	}
+
+	input_report_key(input, BTN_C, registers.c);
+	input_report_key(input, BTN_Z, registers.z);
+	input_sync(input);
+}
+
+static int nunchuk_init(struct nunchuk_device *nunchuk)
+{
+	// Bunch of magic here. Why is it magic? Because the only public documentation on the Wii Nunchuk I2C interface was created by reverse engineers.
+	const char first_command[] = { 0xf0, 0x55 };
+	const char second_command[] = { 0xfb, 0x00 };
+	int result = 0;
+	struct i2c_client *client = nunchuk->client;
+	struct nunchuk_registers registers = { 0 };
+
+	result = i2c_master_send(client, first_command, sizeof(first_command));
+	if (sizeof(first_command) != result) {
+		dev_err(&client->dev,
+			"Failed to send first command to nunchuk!");
+		return result;
+	}
+
+	// See https://www.kernel.org/doc/Documentation/timers/timers-howto.txt
+	usleep_range(900, 1100);
+
+	result =
+		i2c_master_send(client, second_command, sizeof(second_command));
+	if (sizeof(second_command) != result) {
+		dev_err(&client->dev,
+			"Failed to send second command to nunchuk!");
+		return result;
+	}
+
+	result = nunchuk_read_registers(nunchuk->client, &registers);
+	if (0 != result) {
+		return result;
+	}
+
+	return 0;
+}
 
 // Probe--the i2c core has determined that our driver matches `client`.
 static int nunchuk_probe(struct i2c_client *client)
@@ -23,13 +112,37 @@ static int nunchuk_probe(struct i2c_client *client)
 		return -ENOMEM;
 	}
 
+	nunchuk->client = client;
 	nunchuk->input = devm_input_allocate_device(&client->dev);
 	if (NULL == nunchuk->input) {
 		return -ENOMEM;
 	}
 
-	result = input_register_device(nunchuk->input);
 	i2c_set_clientdata(client, nunchuk);
+	input_set_drvdata(nunchuk->input, nunchuk);
+
+	// Set up some basic information about the input device
+	nunchuk->input->name = "Wii Nunchuk";
+	nunchuk->input->id.bustype = BUS_I2C;
+
+	// Tell the input subsystem which events we generate
+	set_bit(EV_KEY, nunchuk->input->evbit);
+	set_bit(BTN_C, nunchuk->input->keybit);
+	set_bit(BTN_Z, nunchuk->input->keybit);
+
+	input_setup_polling(nunchuk->input, nunchuk_poll);
+	input_set_poll_interval(nunchuk->input, 20);
+	dev_info(&client->dev, "Nunchuk polling at %d msec",
+		 input_get_poll_interval(nunchuk->input));
+
+	// Initialize the Nunchuk
+	result = nunchuk_init(nunchuk);
+	if (0 != result) {
+		return result;
+	}
+
+	// Must be ready to start polling by this point!
+	result = input_register_device(nunchuk->input);
 	return result;
 }
 
